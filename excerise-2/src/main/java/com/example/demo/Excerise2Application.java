2735ecb556d732f0d382fefb9d29fbb07cface4b
Kaichi Iwahara <umichi1222@gmail.com>
2024-07-10 05:19:10 +0900

    Initial Commit

---
+++ excerise-2/src/main/java/com/example/demo/Excerise2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Excerise2Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Excerise2Application.class, args);
+	}
+
+}
